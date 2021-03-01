88687a9b0150bc82a4047c7422b4b13cd3ec4b95
Minh Doan Quang <dquangminh98@gmail.com>
2021-03-01 22:51:08 +0700

    Update Api Format

--- render_static_api.py
+++ render_static_api.py
@@ -26,11 +26,11 @@ if __name__ == '__main__':
 
     for tafe in tafe_data:
         # schools.append(data_row_to_dict(tafe, type='TAFE', url=api_url))
-        schools[f'{tafe[0]}'] = f'{api_url}/"TAFE"/{tafe[1]}'
+        schools[f'{tafe[0]}'] = f'{api_url}/TAFE/{tafe[1]}'
 
     for uni in uni_data:
         # schools.append(data_row_to_dict(uni, type='UNI', url=api_url))
-        schools[f'{uni[0]}'] = f'{api_url}/"TAFE"/{uni[1]}'
+        schools[f'{uni[0]}'] = f'{api_url}/UNI/{uni[1]}'
     
     # for school in schools:
     #     with open(f'{root_folder}/{school["name"]}.json', 'w') as jsonfile:
