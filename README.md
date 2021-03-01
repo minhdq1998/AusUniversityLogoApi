0ba6ae70262826bfd276d9c830e44e34561e623e
Minh Doan Quang <dquangminh98@gmail.com>
2021-03-01 17:19:32 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ### Australian University Logo Api
 
 Allow you to retrieve logo of Australian University by giving the corect University Name
+
+API_URL
+
+test api
+```
+curl https://raw.githubusercontent.com/minhdq1998/AusUniversityLogoApi/main/api/all.json
+```
--- render_static_api.py
+++ render_static_api.py
@@ -10,24 +10,26 @@ def read_data_csv(filename):
             result.append(row)
     return result
 
-def data_row_to_dict(row, type):
+def data_row_to_dict(row, type, url):
     return {
         'name': row[0],
-        'logo': f'/{type}/{row[1]}'
+        'logo': f'{url}/{type}/{row[1]}'
     }
 
 if __name__ == '__main__':
+    api_url = 'https://raw.githubusercontent.com/minhdq1998/AusUniversityLogoApi/main'
     root_folder = 'api/'
+
     tafe_data = read_data_csv('tafe.csv')
     uni_data = read_data_csv('uni.csv')
 
     schools = []
 
     for tafe in tafe_data:
-        schools.append(data_row_to_dict(tafe, type='TAFE'))
+        schools.append(data_row_to_dict(tafe, type='TAFE', url=api_url))
 
     for uni in uni_data:
-        schools.append(data_row_to_dict(uni, type='UNI'))
+        schools.append(data_row_to_dict(uni, type='UNI', url=api_url))
     
     for school in schools:
         with open(f'{root_folder}/{school["name"]}.json', 'w') as jsonfile:
